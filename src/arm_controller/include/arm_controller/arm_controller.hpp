44febafa6d9c6045d44d244866ee3b87223b1aa5
Allen Abraham <allenabraham106@gmail.com>
2026-05-26 11:07:45 -0600

    arm_controller nodes created

---
+++ src/arm_controller/include/arm_controller/arm_controller.hpp
@@ -0,0 +1,17 @@
+#pragma once 
+#include <rclcpp/rclcpp.hpp>
+#include <moveit/move_group_interface/move_group_interface.hpp>
+#include <geometry_msgs/msg/pose.hpp>
+#include <memory>
+
+class ArmController{
+    public: 
+        explicit ArmContoller(const rclcpp::Node::SharedPtr & node);
+        bool initialized(); 
+        bool moveToPose(double x, double y, double z);
+        bool stop();
+    
+    private:
+        rclcpp::Node::SharedPtr node_; // shared pointer for a ros2 node 
+        std::shared_ptr<moveit::planning_interface::MoveGroupInterface> move_group_; // shared pointer to the MoveGroupInterface
+}
---
+++ src/arm_controller/src/arm_controller.cpp
@@ -0,0 +1,31 @@
+#include "arm_controller/arm_controller.hpp"
+
+ArmController::ArmController(const rclcpp::Node::SharedPtr & ptr) : node_(node){
+
+}
+
+bool ArmController::Initialize(){
+    move_group_ = std::make_shared<moveit:planning_interface::MoveGroupInterface>(
+        node_ = "panada_arm"
+    ); 
+    RCLCPP_INFO(node_->get_logger(), "ArmController Initialized");
+    return true; 
+}
+
+bool ArmController::moveToPose(double x, double y, double z){
+    geometry_msgs::msg::Pose target_pose; 
+    target_pose.position.x = x;
+    target_pose.position.y = y;
+    target_pose.position.z = z; 
+    target_pose.orientation.w = 1.0;
+
+    move_group_->setPoseTarget(target_pose); 
+    auto result = move_group_->move();
+    return result == moveit::core::MoveItErrorCode::SUCCESS;
+}
+
+bool ArmController::stop(){
+    move_group_->stop();
+    RCLCPP_INFO(node_->get_logger(), "ArmController Stopped")
+    return true;
+}
