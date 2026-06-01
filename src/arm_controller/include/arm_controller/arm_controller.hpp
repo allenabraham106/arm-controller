a6fea2d787507416c921b38bbb78f1c19a2b5e27
Allen Abraham <allenabraham106@gmail.com>
2026-06-01 05:03:18 -0600

    addressing PR comments

--- src/arm_controller/include/arm_controller/arm_controller.hpp
+++ src/arm_controller/include/arm_controller/arm_controller.hpp
@@ -1,9 +1,11 @@
 #pragma once 
 #include <rclcpp/rclcpp.hpp>
 #include <moveit/move_group_interface/move_group_interface.h>
-#include <geometry_msgs/msg/pose.hpp>
-#include <geometry_msgs/msg/point_stamped.hpp>
-#include <geometry_msgs/msg/pose_stamped.hpp>
+
+//ROS2 interface for target arm pose and input
+#include <geometry_msgs/msg/pose.hpp>           // target end effector pose
+#include <geometry_msgs/msg/point_stamped.hpp>  // clicked point in RViz
+#include <geometry_msgs/msg/pose_stamped.hpp>   // target pose from GUI
 #include <memory>
 #include <atomic>
 
--- src/arm_controller/src/arm_controller.cpp
+++ src/arm_controller/src/arm_controller.cpp
@@ -7,13 +7,19 @@ ArmController::ArmController(const rclcpp::Node::SharedPtr & node) : node_(node)
 
 bool ArmController::initialize(){
     move_group_ = std::make_shared<moveit::planning_interface::MoveGroupInterface>(
-        node_,"panda_arm"
+        node_, "panda_arm"
     ); 
+
+    // Subscribes to clicked points from RViz"publish point" tool 
+    // Use case: manual point selection in RViz
     clicked_point_sub_ = node_->create_subscription<geometry_msgs::msg::PointStamped>(
         "/clicked_point", 
         10, 
         std::bind(&ArmController::onClickedPoint, this, std::placeholders::_1)
     );
+
+    // Subscribers to slider inputs from the GUI inputs
+    // Use case: precise control of position and orientation from the GUI 
     target_pose_sub_ = node_->create_subscription<geometry_msgs::msg::PoseStamped>(
         "/arm_target_pose", 
         10, 
@@ -25,6 +31,7 @@ bool ArmController::initialize(){
 }
 
 bool ArmController::moveToPose(double x, double y, double z){
+    is_moving_ = true;
     geometry_msgs::msg::Pose target_pose; 
     target_pose.position.x = x;
     target_pose.position.y = y;
@@ -37,14 +44,17 @@ bool ArmController::moveToPose(double x, double y, double z){
     auto plan_result = move_group_->plan(plan);
     if(plan_result != moveit::core::MoveItErrorCode::SUCCESS){
         RCLCPP_ERROR(node_->get_logger(), "Planning with failed node: %d", plan_result.val);
+        is_moving_ = false;
         return false;
     }
 
     auto exec_result = move_group_->execute(plan);
     if(exec_result != moveit::core::MoveItErrorCode::SUCCESS){
         RCLCPP_ERROR(node_->get_logger(), "Execution failed with node: %d", exec_result.val);
+        is_moving_ = false;
         return false;
     }
+    is_moving_ = false; 
     return true;
 }
 
@@ -59,11 +69,7 @@ void ArmController::onClickedPoint(const geometry_msgs::msg::PointStamped::Share
         RCLCPP_INFO(node_->get_logger(), "Already moving, ignoring click");
         return;
     }
-    is_moving_ = true; 
-    RCLCPP_INFO(node_->get_logger(), "Recieved Point: x=%.2f y=%.2f z=%.2f",
-                    msg->point.x, msg->point.y, msg->point.z);
     moveToPose(msg->point.x, msg->point.y, msg->point.z);
-    is_moving_ = false;
 }
 
 void ArmController::onTargetPose(const geometry_msgs::msg::PoseStamped::SharedPtr msg){
@@ -71,9 +77,7 @@ void ArmController::onTargetPose(const geometry_msgs::msg::PoseStamped::SharedPt
         RCLCPP_INFO(node_->get_logger(), "Already moving, ignoring");
         return;
     }
-    is_moving_ = true;
     RCLCPP_INFO(node_->get_logger(), "GUI target: x=%.2f y=%.2f z=%.2f",
         msg->pose.position.x, msg->pose.position.y, msg->pose.position.z);
     moveToPose(msg->pose.position.x, msg->pose.position.y, msg->pose.position.z);
-    is_moving_ = false;
 }
