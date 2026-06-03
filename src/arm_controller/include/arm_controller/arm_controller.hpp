50d49e9238c5a42a39b2cf1cb787e6a42b67d312
Allen Abraham <allenabraham106@gmail.com>
2026-06-03 11:57:01 -0600

    cleaning up the code and addressing final PR comments

--- src/arm_controller/include/arm_controller/arm_controller.hpp
+++ src/arm_controller/include/arm_controller/arm_controller.hpp
@@ -2,9 +2,9 @@
 #include <rclcpp/rclcpp.hpp>
 // only using .h rather than .hpp for CI compatibility. For jazzy and newer .hpp is fine
 #include <moveit/move_group_interface/move_group_interface.h> 
-#include <geometry_msgs/msg/pose.hpp>           // target end effector pose
-#include <geometry_msgs/msg/point_stamped.hpp>  // clicked point in RViz
-#include <geometry_msgs/msg/pose_stamped.hpp>   // target pose from GUI
+#include <geometry_msgs/msg/pose.hpp>
+#include <geometry_msgs/msg/point_stamped.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
 #include <std_msgs/msg/empty.hpp>
 #include <memory>
 #include <atomic>
